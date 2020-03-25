692626855b9d9e3af30b5e6018ef1102b0377869
BGottschalk <55898311+BGottschalk@users.noreply.github.com>
2020-03-25 02:46:26 -0700

    add pblTrainer to website

--- functions/index.js
+++ functions/index.js
@@ -10,10 +10,7 @@ app.engine('handlebars', handlebars.engine);
 app.use(bodyParser.urlencoded({extended:true}));
 app.use('/static', express.static('../public'));
 app.set('view engine', 'handlebars');
-// app.set('port', process.argv[2]);
-
-// app.use('/', express.static('../public'));
-// app.use('/comps', require('./comps.js'));
+app.set('port', process.argv[2]);
 
 app.use('/', express.static('../public'));
 
@@ -21,14 +18,16 @@ app.get('/comps', function(req, res){
   fetch('https://www.worldcubeassociation.org/api/v0/users/6836?upcoming_competitions=true')
     .then(response => response.json())
     .then(data => {
-      // console.log(data);
       var context = data;
-      // console.log(context);
 
       var handlebars_file = 'compPage';
       res.render(handlebars_file, context);
     })
     .catch(err => console.error(err));
+});
+
+app.get('/pblTrainer', function(req, res) {
+  res.render('pblTrainer')
 });
 
 app.use(function(req,res){
@@ -42,8 +41,9 @@ app.use(function(err, req, res, next){
   res.render('500');
 });
 
-// app.listen(app.get('port'), function(){
-//   console.log('Express started on http://localhost:' + app.get('port') + '; press Ctrl-C to terminate.');
-// });
+app.listen(app.get('port'), function(){
+  console.log('Express started on http://localhost:' + app.get('port') + '; press Ctrl-C to terminate.');
+});
 
 exports.comps = functions.https.onRequest(app);
+exports.pblTrainer = functions.https.onRequest(app);
